a4a711e5a3d0f6d95b5744bcd574aaf48cc072dc
Xuan Hieu <xuanhieu1479@gmail.com>
2019-04-28 19:23:32 +0700

    Insert hash instead of raw password to db from admin seeder

--- database/seeds/nguoidungTableSeeder.php
+++ database/seeds/nguoidungTableSeeder.php
@@ -11,9 +11,10 @@ class nguoidungTableSeeder extends Seeder
      */
     public function run()
     {
+        $hash = password_hash('123', PASSWORD_DEFAULT);
         DB::table('nguoidung')->insert([
             'email' => 'admin@gmail.com',
-            'matkhau' => '123',
+            'matkhau' => $hash,
             'ten' => 'Admin',
             'sdt' => '1234567890',
             'idchucvu' => 1,

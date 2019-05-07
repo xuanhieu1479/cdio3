b7bfcd1438fb13cd9ee9b8d7d08ddc709e405203
Xuan Hieu <xuanhieu1479@gmail.com>
2019-05-08 04:20:40 +0700

    Add phong seeder

---
+++ database/seeds/phongTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class nguoidungTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('phong')->insert([
+            'idphong' => 'abc321',
+            'idhomestay' => 'abc123',
+            'songuoitoida' => 1,
+            'gia' => 800000,
+            'thongtin' => 'Blahblahlbah',
+            'thumbnail' => "http://cdn.sandals.com/sandals/v12/images/resorts/sbd/home/main-slider/pool-with-swim-up-bar-beach.jpg",
+            'giamgia' => 0,
+            'tinhtrang' => 'Bình thường',
+        ]);
+
+        DB::table('homestay')->insert([
+            'idphong' => 'cef456',
+            'idhomestay' => 'cef654',
+            'songuoitoida' => 2,
+            'gia' => 1500000,
+            'thongtin' => 'Blahblahlbah',
+            'thumbnail' => "http://cdn.sandals.com/sandals/v12/images/resorts/sbd/home/main-slider/pool-with-swim-up-bar-beach.jpg",
+            'giamgia' => 0,
+            'tinhtrang' => 'Bình thường',
+        ]);
+
+        DB::table('homestay')->insert([
+            'idphong' => 'ghk987',
+            'idhomestay' => 'ghk789',
+            'songuoitoida' => 3,
+            'gia' => 2400000,
+            'thongtin' => 'Blahblahlbah',
+            'thumbnail' => "http://cdn.sandals.com/sandals/v12/images/resorts/sbd/home/main-slider/pool-with-swim-up-bar-beach.jpg",
+            'giamgia' => 0,
+            'tinhtrang' => 'Bình thường',
+        ]);
+    }
+}
